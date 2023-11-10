1cf859a532746feb38c57477a95b59c7e55287ac
DarioGuastella <jimmyguastella@gmail.com>
2023-11-10 16:17:24 +0100

    feat: crea array e stampa info in console

--- js/main.js
+++ js/main.js
@@ -1,1 +1,40 @@
-console.log("prova");
+//CREO ARRAY CON OGGETTO PER OGNI MEMBRO DEL TEAM
+const ourTeam = [
+    {
+        nome: "Wayne Barnett",
+        ruolo: "Founder & CEO",
+        foto:"wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        nome: "Angela Caroll",
+        ruolo: "Chief Editor",
+        foto:"angela-caroll-chief-editor.jpg"
+    },
+    {
+        nome: "Walter Gordon",
+        ruolo: "Office Manager",
+        foto:"walter-gordon-office-manager.jpg"
+    },
+    {
+        nome: "Angela Lopez",
+        ruolo: "Social Media Manager",
+        foto:"angela-lopez-social-media-manager.jpg"
+    },
+    {
+        nome: "Scott Estrada",
+        ruolo: "Developer",
+        foto:"scott-estrada-developer.jpg"
+    },
+    {
+        nome: "Barbara Ramos",
+        ruolo: "Graphic Designer",
+        foto:"barbara-ramos-graphic-designer.jpg"
+    },
+
+]
+console.log("Our Team Member List:")
+for (let i = 0; i < ourTeam.length; i++) {
+    for (let key in ourTeam[i]) {
+        console.log(`Membro numero ${[i]} ${key}: `, ourTeam[i][key]);
+    }
+}
