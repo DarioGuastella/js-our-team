ef1e2f605107032df67679ba64d6ea2a471aa463
DarioGuastella <jimmyguastella@gmail.com>
2023-11-13 10:40:17 +0100

    feat: sposta img/ nel ciclo

--- js/main.js
+++ js/main.js
@@ -4,32 +4,32 @@ const ourTeam = [
     {
         nome: "Wayne Barnett",
         ruolo: "Founder & CEO",
-        foto:"img/wayne-barnett-founder-ceo.jpg"
+        foto:"wayne-barnett-founder-ceo.jpg"
     },
     {
         nome: "Angela Caroll",
         ruolo: "Chief Editor",
-        foto:"img/angela-caroll-chief-editor.jpg"
+        foto:"angela-caroll-chief-editor.jpg"
     },
     {
         nome: "Walter Gordon",
         ruolo: "Office Manager",
-        foto:"img/walter-gordon-office-manager.jpg"
+        foto:"walter-gordon-office-manager.jpg"
     },
     {
         nome: "Angela Lopez",
         ruolo: "Social Media Manager",
-        foto:"img/angela-lopez-social-media-manager.jpg"
+        foto:"angela-lopez-social-media-manager.jpg"
     },
     {
         nome: "Scott Estrada",
         ruolo: "Developer",
-        foto:"img/scott-estrada-developer.jpg"
+        foto:"scott-estrada-developer.jpg"
     },
     {
         nome: "Barbara Ramos",
         ruolo: "Graphic Designer",
-        foto:"img/barbara-ramos-graphic-designer.jpg"
+        foto:"barbara-ramos-graphic-designer.jpg"
     }
 
 ]
@@ -46,7 +46,7 @@ for (let i = 0; i < ourTeam.length; i++) {
     const card = document.createElement("div");
     card.classList.add("card", "mb-4")
     const image = document.createElement("img");
-    image.src = `${member.foto}`;
+    image.src = `img/${member.foto}`;
     const memberName = document.createElement("h6");
     memberName.innerHTML = `${member.nome}`;
     memberName.classList.add("my-3");
